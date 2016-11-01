1c86bda0c3e6f1d105560fc6cc80d195ff5136f9
MurphysL <1619673065@qq.com>
2016-11-01 14:30:00 +0800

    必应图片加载

--- todo/src/main/java/com/example/murphysl/todo/MainActivity.java
+++ todo/src/main/java/com/example/murphysl/todo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.murphysl.todo;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,11 +8,13 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -81,6 +84,39 @@ public class MainActivity extends AppCompatActivity{
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void changeColor(Palette.Swatch s){
+
+        tabLayout.setBackgroundColor(s.getRgb());
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            // 很明显，这两货是新API才有的。
+            window.setStatusBarColor(colorBurn(s.getRgb()));
+            window.setNavigationBarColor(colorBurn(s.getRgb()));
+        }
+    }
+
+    /**
+     * 颜色加深处理
+     *
+     * @param RGBValues
+     *            RGB的值，由alpha（透明度）、red（红）、green（绿）、blue（蓝）构成，
+     *            Android中我们一般使用它的16进制，
+     *            例如："#FFAABBCC",最左边到最右每两个字母就是代表alpha（透明度）、
+     *            red（红）、green（绿）、blue（蓝）。每种颜色值占一个字节(8位)，值域0~255
+     *            所以下面使用移位的方法可以得到每种颜色的值，然后每种颜色值减小一下，在合成RGB颜色，颜色就会看起来深一些了
+     * @return
+     */
+    private int colorBurn(int RGBValues) {
+        int alpha = RGBValues >> 24;
+        int red = RGBValues >> 16 & 0xFF;
+        int green = RGBValues >> 8 & 0xFF;
+        int blue = RGBValues & 0xFF;
+        red = (int) Math.floor(red * (1 - 0.1));
+        green = (int) Math.floor(green * (1 - 0.1));
+        blue = (int) Math.floor(blue * (1 - 0.1));
+        return Color.rgb(red, green, blue);
     }
 
 }
--- todo/src/main/java/com/example/murphysl/todo/PlanFragment.java
+++ todo/src/main/java/com/example/murphysl/todo/PlanFragment.java
@@ -1,14 +1,23 @@
 package com.example.murphysl.todo;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.graphics.Palette;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
-import com.squareup.picasso.Picasso;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.squareup.picasso.Transformation;
+
+import java.io.IOException;
 
 /**
  * PlanFragment
@@ -17,33 +26,82 @@ import com.squareup.picasso.Picasso;
  * @time: 2016/10/30 10:59
  */
 
-public class PlanFragment extends Fragment{
+public class PlanFragment extends Fragment implements Transformation{
 
     private ImageView imageView;
+    private boolean hasMeasured = false;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.plan_fragment , container , false);
         imageView = (ImageView) root.findViewById(R.id.image);
+        ViewTreeObserver vto = root.getViewTreeObserver();
+
+        vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            public boolean onPreDraw() {
+                if (hasMeasured == false) {
+                    int height = imageView.getMeasuredHeight();
+                    int width = imageView.getMeasuredWidth();
+
+                    try {
+                        initPic(height , width);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    hasMeasured = true;
+                }
+                return true;
+            }
+        });
         return root;
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private void initPic(int height , int width) throws IOException {
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        initPic();
-    }
+        Log.i("pic", "initPic: " + height + width);
 
-    private void initPic() {
-        //getContext!!!!
-        Picasso.with(getContext())
+        Glide.with(this)
                 .load("http://tu.ihuan.me/api/bing")
+                .fitCenter()
+                .crossFade() //设置淡入淡出效果，默认300ms，可以传参
                 .into(imageView);
+
+        //getContext!!!!
+        Glide.with(this)
+                .load("http://tu.ihuan.me/api/bing")
+                .asBitmap()
+                .override(width , height)
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                        //imageView.setImageBitmap(resource);
+                        Palette.from(resource).generate(new Palette.PaletteAsyncListener() {
+                            @Override
+                            public void onGenerated(Palette palette) {
+                                Palette.Swatch s1 = palette.getVibrantSwatch();
+                                ((MainActivity)getActivity()).changeColor(s1);
+                            }
+                        });
+                    }
+        });
+
+    }
+
+    @Override
+    public Bitmap transform(Bitmap source) {
+        int size = Math.min(source.getWidth(), source.getHeight());
+        int x = (source.getWidth() - size) / 2;
+        int y = (source.getHeight() - size) / 2;
+        Bitmap result = Bitmap.createBitmap(source, x, y, size, size);
+        if (result != source) {
+            source.recycle();
+        }
+        return result;
+    }
+
+    @Override
+    public String key() {
+        return null;
     }
 }
